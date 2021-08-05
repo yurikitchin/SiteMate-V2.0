d46b42f5c14132ef0c2c1850ec48d80877d7e07f
Yuri Kitchin <ykitchin@gmail.com>
2021-08-05 22:00:04 +0800

    add employee model

---
+++ server/models/employee.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const employeeSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  phone:{
+    type: Number,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  isManager: [
+    {
+      type: Boolean,
+    },
+  ],
+});
+
+// set up pre-save middleware to create password
+employeeSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+employeeSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const Employee = model('Employee', employeeSchema);
+
+module.exports = Employee;
